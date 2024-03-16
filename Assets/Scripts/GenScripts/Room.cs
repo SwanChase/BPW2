b2debc48e8d4e2ace23d0380ec65ec3c80462d88
Swan Chase <swancd@gmail.com>
2024-03-16 18:39:44 +0100

    1st attempt at DungGen

--- Assets/Scripts/GenScripts/Room.cs
+++ Assets/Scripts/GenScripts/Room.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+
+    public int Width;
+    public int Height;
+    public int X;
+    public int Y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (RoomController.Instance == null)
+        {
+            Debug.Log("Dafug u doin in the wrong scene");
+            return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCenter()
+    {
+        return new Vector3(X * Width, Y * Height);
     }
 }
--- Assets/Scripts/GenScripts/RoomController.cs
+++ Assets/Scripts/GenScripts/RoomController.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomInfo
+{
+    public string name;
+    public int x;
+    public int y;
+}
 
 public class RoomController : MonoBehaviour
 {
+
+    public static RoomController Instance;
+
+    string currentWorldName = "Main";
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+
+    public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
